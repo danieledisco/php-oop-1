111c5447b6418bc6dca021efe2da6a495b3fe2a0
danieledisco <daniele.disco.1965@gmail.com>
2024-03-16 12:05:15 +0100

    Generated data file

---
+++ app/Data/db.php
@@ -0,0 +1,110 @@
+<?php
+
+$movieData = [
+    [
+        'title' => 'Titanic 1',
+        'movieGenre' => 'Adventure-Dramatic',
+        'yearProduction' => 1977,
+        'countryProduction' => 'USA',
+        'duration' => 194,
+        'sponsor' => 'Paramaut Pictures',
+        'producer' => 'Rea Sanchini',
+        'director' => 'James Cameron',
+        'screePlayer' => 'James Cameron',
+        'photograpyDirector' => 'Russell Carpenter',
+        'mainActor' => 'Leonardo Di Caprio'
+    ],
+    [
+        'title' => 'Titanic 2',
+        'movieGenre' => 'Adventure-Dramatic',
+        'yearProduction' => 1977,
+        'countryProduction' => 'USA',
+        'duration' => 194,
+        'sponsor' => 'Paramaut Pictures',
+        'producer' => 'Rea Sanchini',
+        'director' => 'James Cameron',
+        'screePlayer' => 'James Cameron',
+        'photograpyDirector' => 'Russell Carpenter',
+        'mainActor' => 'Leonardo Di Caprio'
+    ],
+    [
+        'title' => 'Titanic 3',
+        'movieGenre' => 'Adventure-Dramatic',
+        'yearProduction' => 1977,
+        'countryProduction' => 'USA',
+        'duration' => 194,
+        'sponsor' => 'Paramaut Pictures',
+        'producer' => 'Rea Sanchini',
+        'director' => 'James Cameron',
+        'screePlayer' => 'James Cameron',
+        'photograpyDirector' => 'Russell Carpenter',
+        'mainActor' => 'Leonardo Di Caprio'
+    ],
+    [
+        'title' => 'Titanic 4',
+        'movieGenre' => 'Adventure-Dramatic',
+        'yearProduction' => 1977,
+        'countryProduction' => 'USA',
+        'duration' => 194,
+        'sponsor' => 'Paramaut Pictures',
+        'producer' => 'Rea Sanchini',
+        'director' => 'James Cameron',
+        'screePlayer' => 'James Cameron',
+        'photograpyDirector' => 'Russell Carpenter',
+        'mainActor' => 'Leonardo Di Caprio'
+    ],
+    [
+        'title' => 'La vita è bella 1',
+        'movieGenre' => 'Comic-Dramatic',
+        'yearProduction' => 1977,
+        'countryProduction' => 'Italia',
+        'duration' => 122,
+        'sponsor' => 'Melampo',
+        'producer' => 'Elda Ferri',
+        'director' => 'Roberto Benigni',
+        'screePlayer' => 'Vincenzo Cerami',
+        'photograpyDirector' => 'Tonino Delli Colli',
+        'mainActor' => 'Roberto Benigni'
+    ],
+    [
+        'title' => 'La vita è bella 1',
+        'movieGenre' => 'Comic-Dramatic',
+        'yearProduction' => 1977,
+        'countryProduction' => 'Italia',
+        'duration' => 122,
+        'sponsor' => 'Melampo',
+        'producer' => 'Elda Ferri',
+        'director' => 'Roberto Benigni',
+        'screePlayer' => 'Vincenzo Cerami',
+        'photograpyDirector' => 'Tonino Delli Colli',
+        'mainActor' => 'Roberto Benigni'
+    ],
+
+    [
+        'title' => 'La vita è bella 1',
+        'movieGenre' => 'Comic-Dramatic',
+        'yearProduction' => 1977,
+        'countryProduction' => 'Italia',
+        'duration' => 122,
+        'sponsor' => 'Melampo',
+        'producer' => 'Elda Ferri',
+        'director' => 'Roberto Benigni',
+        'screePlayer' => 'Vincenzo Cerami',
+        'photograpyDirector' => 'Tonino Delli Colli',
+        'mainActor' => 'Roberto Benigni'
+    ],
+    [
+        'title' => 'La vita è bella 1',
+        'movieGenre' => 'Comic-Dramatic',
+        'yearProduction' => 1977,
+        'countryProduction' => 'Italia',
+        'duration' => 122,
+        'sponsor' => 'Melampo',
+        'producer' => 'Elda Ferri',
+        'director' => 'Roberto Benigni',
+        'screePlayer' => 'Vincenzo Cerami',
+        'photograpyDirector' => 'Tonino Delli Colli',
+        'mainActor' => 'Roberto Benigni'
+    ]
+
+];
